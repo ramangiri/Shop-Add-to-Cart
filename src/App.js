0587d74f481df0b3d5deadf72524af0b1f351f3f
ramangiri <60056285+ramangiri@users.noreply.github.com>
2021-06-23 19:16:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React from 'react';
+// For getting star value
+import ReactStars from "react-rating-stars-component";  
+import './App.css';
+import Nav from './Nav';
+// data stored in a productdata variable
+import ProductData from './ProductData'; 
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: ProductData,
+      cartCount: 0
+    }
+  }
+
+
+// Add to Cart function 
+  addToCart = (event) => {
+    let index = parseInt(event.target.dataset.id);
+
+    let tempData = this.state.data;
+    tempData[index].cart = true;
+    this.setState({ cartCount: this.state.cartCount + 1 });
+    this.setState({ data: tempData })
+  }
+// Remove the product in Cart function
+  removeFromCart = (event) => {
+    let index = parseInt(event.target.dataset.id);
+
+    let tempData = this.state.data;
+    tempData[index].cart = false;
+
+    if (this.state.cartCount > 0) {
+      this.setState({ cartCount: this.state.cartCount - 1 });
+    }
+
+    this.setState({ data: tempData })
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <Nav count={this.state.cartCount} /> {/* Navbar  */}
+        <header class="bg-dark py-5">
+          <div class="container px-4 px-lg-5 my-5">
+            <div class="text-center text-white">
+              <h1 class="display-4 fw-bolder">RG Shop in style</h1>
+              <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+            </div>
+          </div>
+        </header>
+
+        <div className="container">
+          <div className="row">
+            {
+              this.state.data.map((item, index) => (
+                <div key={item.id} className="col-12 col-sm-12 col-md-4 col-lg-4 pb-2 pt-2">
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" className="card-img-top" alt="cartItem" />
+                    <div className="card-body">
+                      <h5 className="card-title">{item.title}</h5>
+                      <p className="card-text">$ {item.price}</p>
+                      <p className="ps-5 ms-4">
+                        {/* user can give Star Rating according to value   */}
+                         <ReactStars size={30}        
+                            half={false}
+                            onChange={newRating => {
+                            console.log(newRating);
+                          }}
+                        />
+
+
+                      </p>
+                      {
+                        item.cart === false ?
+
+                          <button className="btn btn-dark" data-id={index} onClick={(event) => this.addToCart(event)}>Add to Cart</button>
+                          :
+
+                          <button className="btn btn-danger" data-id={index} onClick={(event) => this.removeFromCart(event)}>Remove from cart</button>
+
+                      }
+
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+
+          </div>
+        </div>
+        <footer class="py-5 bg-dark">
+          <div class="container"><p class="m-0 text-center text-white">Copyright © Raman Giri 2021</p></div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
+
+
+function Nav({ count })   {
+
+    return(
+        <>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <span className="navbar-brand">Start Bootstrap</span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <span className="nav-link active" aria-current="page">Home</span>
+                            </li>
+                            <li className="nav-item">
+                                <span className="nav-link">About</span>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <span className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Shop
+                                </span>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><span className="dropdown-item">Action</span></li>
+                                    <li><span className="dropdown-item">Another action</span></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><span className="dropdown-item">Something else here</span></li>
+                                </ul>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                    <div className="d-flex">
+                        <button className="cart btn btn-light border border-dark" type="button">
+                            {/* FaShoppingCart is cart icon from react, number of count from cart  */}
+                            <FaShoppingCart /> Cart <span className="cart-count">{count}</span>  
+                        </button>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
+export default Nav;
