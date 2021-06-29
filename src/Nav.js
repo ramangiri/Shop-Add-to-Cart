fb8d0f0a125b81fd10bbd0e81fac87acbe83b403
ramangiri <60056285+ramangiri@users.noreply.github.com>
2021-06-29 20:02:33 +0530

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -33,14 +33,15 @@ function Nav({ count })   {
                             </li>
 
                         </ul>
-
-                    </div>
-                    <div className="d-flex">
+         <div className="d-flex">
                         <button className="cart btn btn-light border border-dark" type="button">
                             {/* FaShoppingCart is cart icon from react, number of count from cart  */}
                             <FaShoppingCart /> Cart <span className="cart-count">{count}</span>  
                         </button>
                     </div>
+
+                    </div>
+                   
                 </div>
             </nav>
         </>
