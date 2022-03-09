7f71fc7e1023232464559fa7553347bb633128d4
karthick <karthickkrishnamoorthi94@gmail.com>
2022-03-09 13:46:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends React.Component {
         <header className="bg-dark py-5">
           <div className="container px-4 px-lg-5 my-5">
             <div className="text-center text-white">
-              <h1 className="display-4 fw-bolder">RG Shop in style</h1>
+              <h1 className="display-4 fw-bolder"> Shop in style</h1>
               <p className="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
             </div>
           </div>
